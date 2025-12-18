c79ad6b5a05d203e1d06d298cc7c7f656aa83d25
omer <ofaruk.yildirim@outlook.com>
2025-12-19 00:34:39 +0300

    final

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,29 @@
 `use strict`;
+
+
+function getShippingCost(country) {
+    let price;
+    switch (country) {
+        case "China":
+            price = 100;
+            break;
+        case "Chile":
+            price = 250;
+            break;
+        case "Australia":
+            price = 170;
+            break;
+        case "Jamaica":
+            price = 120;
+            break
+        default:
+            price = 0;
+    }
+
+    if (price) {
+        return `Shipping to ${country} will cost ${price} credits`;
+    } else {
+        return "Sorry, there is no delivery to your country"
+    }
+}
+
