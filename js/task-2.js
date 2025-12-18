3a5b882e12423ec449057d9ee1fade71ecbe0e8f
omer <ofaruk.yildirim@outlook.com>
2025-12-19 00:15:07 +0300

    task-3

--- js/task-2.js
+++ js/task-2.js
@@ -1,2 +1,12 @@
 `use strict`;
 
+function formatMessage(message, maxLength) {
+    let newMessage;
+    if (message.length <= maxLength) {
+        return message
+    } else {
+        newMessage = message.slice(0, message.length);
+        newMessage = newMessage + "..."
+        return newMessage;
+    }
+}
--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,7 @@
 `use strict`;
+
+function checkForSpam(message) {
+    const control = message.toLowerCase().includes("spam") || message.toLowerCase().includes("sale")
+    return control;
+}
+
