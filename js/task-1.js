ead62dc60072b6045545ca6d877b0b6fb2c78805
omer <ofaruk.yildirim@outlook.com>
2025-12-18 18:20:25 +0300

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,11 @@
 `use strict`;
 
 
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+    let totalPrice = quantity * pricePerDroid;
+    if (totalPrice > customerCredits) {
+        return 'Insufficient funds!'
+    } else {
+        return `You ordered ${quantity} droids worth ${totalPrice} credits!`
+    } 
+}
